6e54a563c1b9f836b64a27fd0f29014560bf74a2
einartech <imalovelyghost13@gmail.com>
2025-01-10 16:04:01 +0100

    imports and exports func done, also some basic of dom interaction

--- src/js/api.js
+++ src/js/api.js
@@ -1,29 +1,29 @@
-console.log("batman");
+import { showRandomText } from "./dom.js";
 
 async function getRandomFacts() {
   const url = "https://uselessfacts.jsph.pl/api/v2/facts/random";
-  
+
   try {
     const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
     const json = await response.json();
-    console.table(json);
+    // console.table(json);
 
     let curiousFactRandomId = json.id;
-    let curiousFactRandomText= json.text;
-    let curiousFactRandomSource= json.source;
-    let curiousFactRandomSourceUrl= json.source_url;
-    let curiousFactRandomLenguage= json.language;
-    let curiousFactRandomPermaLink= json.permalink;
+    let curiousFactRandomText = json.text;
+    let curiousFactRandomSource = json.source;
+    let curiousFactRandomSourceUrl = json.source_url;
+    let curiousFactRandomLenguage = json.language;
+    let curiousFactRandomPermaLink = json.permalink;
 
-  }
-  
-  catch (error) {
+    showRandomText(curiousFactRandomText);
+  } catch (error) {
     console.error(error.message);
   }
 }
 
-getRandomFacts()
+getRandomFacts();
---
+++ src/js/dom.js
@@ -0,0 +1,6 @@
+function showRandomText(apiRandomText) {
+  console.log("showRandomText () >> OUTPUT >>");
+  console.table(apiRandomText);
+}
+
+export { showRandomText };
